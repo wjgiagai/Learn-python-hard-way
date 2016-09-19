cbeddf1dcd1ac6059a33c2f47eba1d66af7dd83e
wjgiagia <wjgiagia@outlook.com>
2016-09-19 22:40:51 +0800

    Create pound.py
    掌握#的注解用法

---
+++ pound.py
@@ -0,0 +1,4 @@
+#a comment,this is sou you can read your program later.
+#anything after the # is ignored by pythhon
+print "i could code like this"#and the comment after is ignored
+print "this will run"
