019023f09c0edc7420b6220779799f9a5cf628f0
wjgiagia <wjgiagia@outlook.com>
2016-09-19 22:45:02 +0800

    Create math.py

---
+++ math.py
@@ -0,0 +1,10 @@
+print "I will now count my chickens:"
+print "hens," ,25+30/6
+print "roosters,", 100-25*3%4
+print "now i will count the eggs"
+print 3+2+1-5+4%2-1/4+6
+print "is it true 3+2 <5-7?"
+print 3+2<5-7
+print "what is 3 +2 ?", 3+2
+print "what is 5-7 ?" ,5-7
+print "oh that is why it's fault!"#掌握python的基本运算符
